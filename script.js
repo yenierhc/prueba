347ee80041524ef558a556d692a27695525ce6f7
Yenier <yenierhc@gmail.com>
2019-05-05 03:12:10 -0600

    1
    1

--- script.js
+++ script.js
@@ -8,28 +8,3 @@ document.onkeydown = function(e){
 	alert(tecla)
 	if (tecla = 116) {return false;}
 };
-( function (){
-			let segundosTimer = 10;
-			timer();
-		function timer(){
-			let segundosTotales = segundosTimer; //30 minutos
-			horas = Math.floor(segundosTotales / 3600);
-			horas = ((horas <  10)? "0" : "") + horas;
-			segundosTotales %= 3600;
-			minutos =  Math.floor(segundosTotales / 60);
-			minutos = ((minutos < 10)? "0":"") + minutos;
-			segundosTotales %= 60;
-			segundosTotales = ((segundosTotales<10)?"0":"")+segundosTotales;
-			document.getElementById("timer").innerHTML = horas + ":"+ minutos +":"+segundosTotales;
-			console.log(segundosTotales);
-			segundosTimer--;
-			console.log(segundosTotales);
-				if (document.getElementById("timer").innerHTML == "00:00:00") {
-					alert('Hemos terminado el examen pues el tiempo se ha agotado \nSi has recibido este mensaje no has enviado tu cuestionario \nEl sistema le otorgará automáticamente la nota de CERO');
-				}
-			if (segundosTotales > 0) {
-			setTimeout(timer,1000);
-
-			}
-		}
-	})();
