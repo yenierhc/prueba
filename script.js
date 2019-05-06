989f7aa5161f63b4978b2c01d22c769490c6484d
Yenier <yenierhc@gmail.com>
2019-05-06 02:11:52 -0600

    great change
    great change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function noBack(){
+/*function noBack(){
     window.location.hash="no-back-button";
     window.location.hash="Again-No-back-button" //chrome
     window.onhashchange=function(){window.location.hash="no-back-button";}
@@ -7,4 +7,20 @@ document.onkeydown = function(e){
 	tecla = (document.all) ? e.keyCode : e.which;
 	alert(tecla)
 	if (tecla = 116) {return false;}
+};*/
+document.onkeydown = function(e){
+ tecla = (document.all) ? e.keyCode : e.which;
+ alert(tecla)
+ if (tecla == 116){
+ 	if(confirm('Successful Message') == true){
+   alert('Esto no esta permitido');
+   return false; 
+	}else{
+		return false;
+	}
+   }
 };
+window.onbeforeunload = function() {
+  return "¿Desea recargar la página web?";
+};
+
